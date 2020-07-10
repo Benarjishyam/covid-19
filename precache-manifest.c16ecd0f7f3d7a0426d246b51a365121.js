db878dce5440ca043b77b3c40958ed394ce7c060
Benarji <benarji556612@gmail.com>
2020-07-10 09:46:42 +0530

    Updates

---
+++ precache-manifest.c16ecd0f7f3d7a0426d246b51a365121.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1dd3c285295caf4349c685cd0b9c3e9",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "ca51253d508010d71bc5",
+    "url": "/covid-19/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4c18a953730f2dd45431",
+    "url": "/covid-19/static/css/main.6e5f736a.chunk.css"
+  },
+  {
+    "revision": "ca51253d508010d71bc5",
+    "url": "/covid-19/static/js/2.4ef3658c.chunk.js"
+  },
+  {
+    "revision": "e239eabcf2f547f393bf324f798e3cd0",
+    "url": "/covid-19/static/js/2.4ef3658c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c18a953730f2dd45431",
+    "url": "/covid-19/static/js/main.52953feb.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  }
+]);
